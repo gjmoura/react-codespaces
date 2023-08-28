caf2e66574b5cbeeb548c2846e5d84bfc7c02831
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2023-08-28 14:53:35 +0000

    Papangu

---
+++ my-app/src/matheusFei.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const MatheusFei = () => {
+    return(
+<>
+        <img src='https://jconlineimagem.ne10.uol.com.br/imagem/galeria/2018/02/11/8312_avulsa/normal/48e0868aa17389fd24e7c8d020012d9d.jpg'/>
+        <Link to="/">Voltar</Link>
+</>
+
+    )
+}
